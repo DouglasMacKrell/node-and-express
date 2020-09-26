407c41b917b85bec29b0f270618577ca9283fa4c
DouglasMacKrell <douglasmackrell@pursuit.org>
2020-09-26 18:56:57 -0400

    route outline

---
+++ rest_api/Outline.md
@@ -0,0 +1,10 @@
+ROUTES:
+    - 404 handler
+    - 500 handler
+    - /status
+    - /signup
+    - /login
+    - /logout
+    - /token
+    - /forgot password
+    - /reset password
--- rest_api/index.js
+++ rest_api/index.js
@@ -3,11 +3,22 @@ const app = express();
 const PORT = 3000;
 
 app.get("/", (req, res) => {
+  console.log("Hi!");
   res.send("Hello world");
 });
 
-app.get("/test", (req, res) => {
-  res.send("test");
+app.get("/status", (req, res) => {
+  res.status(200).json({
+    message: "OK",
+    status: 200,
+  });
+});
+
+app.post("/signup", (req, res) => {
+  res.status(200).json({
+    message: "OK",
+    status: 200,
+  });
 });
 
 app.listen(PORT, () => {
