37fb7076cf101ec2b6fb604c11a9cd92afb15b86
DouglasMacKrell <douglasmackrell@pursuit.org>
2020-09-26 19:15:47 -0400

    error handling and 404 handling

--- rest_api/index.js
+++ rest_api/index.js
@@ -14,10 +14,65 @@ app.get("/status", (req, res) => {
   });
 });
 
-app.post("/signup", (req, res) => {
+app.post("/signup", (req, res, next) => {
+    next(new Error('test'))
+//   res.status(200).json({
+//     message: "OK",
+//     status: 200,
+//   });
+});
+
+app.post("/login", (req, res) => {
   res.status(200).json({
     message: "OK",
     status: 200,
+  });
+});
+
+app.post("/logout", (req, res) => {
+  res.status(200).json({
+    message: "OK",
+    status: 200,
+  });
+});
+
+app.post("/token", (req, res) => {
+  res.status(200).json({
+    message: "OK",
+    status: 200,
+  });
+});
+
+app.post("/forgot-password", (req, res) => {
+  res.status(200).json({
+    message: "OK",
+    status: 200,
+  });
+});
+
+app.post("/reset-password", (req, res) => {
+  res.status(200).json({
+    message: "OK",
+    status: 200,
+  });
+});
+
+// CATCH ALL OTHER ROUTES 
+
+app.use((req, res) => {
+    res.status(404).json({
+        message: "404 PAGE NOT FOUND",
+        status: 404
+    })
+})
+
+// HANDLE ERRORS
+
+app.use((error, req, res, next) => {
+    console.log(error)
+  res.status(error.status || 500).json({
+    error: error.message,
+    status: 500,
   });
 });
 
