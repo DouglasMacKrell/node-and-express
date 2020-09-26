ea8f097b4d2a291c1b0064d3a98c888011ced8b1
DouglasMacKrell <douglasmackrell@pursuit.org>
2020-09-26 19:30:43 -0400

    routes expanded

--- rest_api/index.js
+++ rest_api/index.js
@@ -15,61 +15,106 @@ app.get("/status", (req, res) => {
 });
 
 app.post("/signup", (req, res, next) => {
-    next(new Error('test'))
-//   res.status(200).json({
-//     message: "OK",
-//     status: 200,
-//   });
+  console.log(req.body);
+  if (!req.body) {
+    res.status(400).json({
+      message: "Invalid body",
+      status: 400,
+    });
+  } else {
+    res.status(200).json({
+      message: "OK",
+      status: 200,
+    });
+  }
 });
 
 app.post("/login", (req, res) => {
-  res.status(200).json({
-    message: "OK",
-    status: 200,
-  });
+  if (!req.body) {
+    res.status(400).json({
+      message: "Invalid body",
+      status: 400,
+    });
+  } else {
+    res.status(200).json({
+      message: "OK",
+      status: 200,
+    });
+  }
 });
 
 app.post("/logout", (req, res) => {
-  res.status(200).json({
-    message: "OK",
-    status: 200,
-  });
+  if (!req.body) {
+    res.status(400).json({
+      message: "Invalid body",
+      status: 400,
+    });
+  } else {
+    res.status(200).json({
+      message: "OK",
+      status: 200,
+    });
+  }
 });
 
 app.post("/token", (req, res) => {
-  res.status(200).json({
-    message: "OK",
-    status: 200,
-  });
+  if (!req.body || !req.body.refreshToken) {
+    res.status(400).json({
+      message: "Invalid body",
+      status: 400,
+    });
+  } else {
+      const { refreshToken } = req.body
+    res.status(200).json({
+      message: `Refresh token requested for token: ${refreshToken}`,
+      status: 200,
+    });
+  }
 });
 
 app.post("/forgot-password", (req, res) => {
-  res.status(200).json({
-    message: "OK",
-    status: 200,
-  });
+    if (!req.body || !req.body.email) {
+      res.status(400).json({
+        message: "Invalid body",
+        status: 400,
+      });
+    } else {
+      const { email } = req.body;
+      res.status(200).json({
+        message: `Forgotten password requested for email: ${email}`,
+        status: 200,
+      });
+    }
 });
 
 app.post("/reset-password", (req, res) => {
-  res.status(200).json({
-    message: "OK",
-    status: 200,
-  });
+    if (!req.body || !req.body.email) {
+      res.status(400).json({
+        message: "Invalid body",
+        status: 400,
+      });
+    } else {
+      const { email } = req.body;
+      res.status(200).json({
+        message: `Password reset requested for email: ${email}`,
+        status: 200,
+      });
+    }
 });
 
-// CATCH ALL OTHER ROUTES 
+// CATCH ALL OTHER ROUTES
 
 app.use((req, res) => {
-    res.status(404).json({
-        message: "404 PAGE NOT FOUND",
-        status: 404
-    })
-})
+  res.status(404).json({
+    message: "404 PAGE NOT FOUND",
+    status: 404,
+  });
+});
 
 // HANDLE ERRORS
 
 app.use((error, req, res, next) => {
-    console.log(error)
+  console.log(error);
   res.status(error.status || 500).json({
     error: error.message,
     status: 500,
